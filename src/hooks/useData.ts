47d378c976ef72e0ad05f4b590f278b61c967d84
DWJ127600 <aa127600842@gmail.com>
2025-04-19 11:11:48 +0800

    Refactor: fetch data with React Query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,49 +1,49 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { AxiosRequestConfig, CanceledError } from "axios";
+// import { useState, useEffect } from "react";
+// import apiClient from "../services/api-client";
+// import { AxiosRequestConfig, CanceledError } from "axios";
 
-interface FetchResponse<T> {
-  count: number;
-  results: T[];
-}
+// // export interface FetchResponse<T> {
+// //   count: number;
+// //   results: T[];
+// // }
 
-const useData = <T>(
-  endpoint: string,
-  requestConfig?: AxiosRequestConfig,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  deps?: any[]
-) => {
-  const [data, setData] = useState<T[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
+// const useData = <T>(
+//   endpoint: string,
+//   requestConfig?: AxiosRequestConfig,
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   deps?: any[]
+// ) => {
+//   const [data, setData] = useState<T[]>([]);
+//   const [error, setError] = useState("");
+//   const [isLoading, setLoading] = useState(false);
 
-  useEffect(
-    () => {
-      const controller = new AbortController();
+//   useEffect(
+//     () => {
+//       const controller = new AbortController();
 
-      setLoading(true);
-      apiClient
-        .get<FetchResponse<T>>(endpoint, {
-          signal: controller.signal,
-          ...requestConfig,
-        })
-        .then((res) => {
-          setData(res.data.results);
-        })
-        .catch((e) => {
-          if (e instanceof CanceledError) return;
-          setError(e.message);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
+//       setLoading(true);
+//       apiClient
+//         .get<FetchResponse<T>>(endpoint, {
+//           signal: controller.signal,
+//           ...requestConfig,
+//         })
+//         .then((res) => {
+//           setData(res.data.results);
+//         })
+//         .catch((e) => {
+//           if (e instanceof CanceledError) return;
+//           setError(e.message);
+//         })
+//         .finally(() => {
+//           setLoading(false);
+//         });
 
-      return () => controller.abort();
-    },
-    deps ? [...deps] : []
-  );
+//       return () => controller.abort();
+//     },
+//     deps ? [...deps] : []
+//   );
 
-  return { data, error, isLoading };
-};
+//   return { data, error, isLoading };
+// };
 
-export default useData;
+// export default useData;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import { FetchResponse } from "../services/api-client";
+import apiClient from "../services/api-client";
+import { Platform } from "./usePlatforms";
 
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+// export interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
 export interface Game {
   id: number;
@@ -16,18 +19,34 @@ export interface Game {
   rating_top: number;
 }
 
+// const useGames = (gameQuery: GameQuery) =>
+//   useData<Game>(
+//     "/games",
+//     {
+//       params: {
+//         genres: gameQuery.genre?.id,
+//         platforms: gameQuery.platform?.id,
+//         ordering: gameQuery.sortOrder,
+//         search: gameQuery.searchText,
+//       },
+//     },
+//     [gameQuery]
+//   );
+
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
